1e73e164705019dff0e0017fb69500762e0303c3
shangmeng <shangmeng@tanzhishuju.com>
2018-09-05 14:42:03 +0800

    update

---
+++ ModuleTest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/2 13:40
+# @Author  : 郑梓斌
+
+import core
+
+if __name__ == '__main__':
+    core.face_merge(src_img='images/model.jpg',
+                    dst_img='images/20171030175254.jpg',
+                    #dst_img='images/test_multi.jpg',
+                    #dst_img='images/test_single.jpg',
+                    out_img='images/output.jpg',
+                    face_area=[50, 30, 500, 485],
+                    alpha=0.75,
+                    k_size=(15, 10),
+                    mat_multiple=0.95)
